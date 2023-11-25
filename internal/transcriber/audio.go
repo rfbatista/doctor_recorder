7f9c0a3612a65dcc2bba80d6a0086406fa5ef76c
Renan Batista <renfbatista@gmail.com>
2023-11-25 11:54:57 -0300

    feat: add ogg decoder

--- internal/transcriber/audio.go
+++ internal/transcriber/audio.go
@@ -4,7 +4,6 @@ import (
 	"doctor_recorder/internal/infrastructure/webrtc"
 	"doctor_recorder/pkg/ffmpeg"
 	"fmt"
-	"os"
 	"strings"
 
 	pion "github.com/pion/webrtc/v4"
@@ -23,44 +22,62 @@ func (t *Transcriber) HandleTracker() webrtc.TrackHandler {
 		codec := track.Codec()
 		if strings.EqualFold(codec.MimeType, pion.MimeTypeOpus) {
 			ff, err := ffmpeg.NewFfmpeg()
+			oggFile, err := OggWriterNew("output.ogg", 48000, 2)
 			if err != nil {
 				t.log.Error("failed to create new ffmpeg")
 				return
 			}
-      ff.Init()
+			ff.Init()
 			defer func() {
 				ff.Close()
+				oggFile.Close()
 			}()
 			t.log.Info("Got Opus track, saving to disk as output.opus (48 kHz, 2 channels)")
-			buffer := make([]byte, bufferSize)
+			// buffer := make([]byte, bufferSize)
 			// var p bytes.Buffer
-			t.log.Info("reading packages")
+			rtpBuf := make([]byte, 1400)
 			for {
-				rtpPacket, _, err := track.ReadRTP()
+				t.log.Info("reading packages")
+				packageSize, _, err := track.Read(rtpBuf)
 				if err != nil {
 					t.log.Error("failed to read track package")
 					return
 				}
-				rtpPacket.MarshalTo(buffer)
-				if len(buffer) >= bufferTreshhold {
-					t.log.Info(fmt.Sprintf("Buffer is nearly full. Current size: %d", len(buffer)))
-					result, err := ff.Read(buffer)
-					if err != nil {
-						t.log.Error("failed to create audio")
-						return
-					}
-					outputFile, err := os.Create("out.mp3") // create new file
-					if err != nil {
-						t.log.Error("failed to create audio file")
-						return
-					}
-					defer outputFile.Close()
-					_, err = outputFile.Write(result.Bytes()) // write result buffer to file
-					if err != nil {
-						t.log.Error("failed to write audio file")
-						return
-					}
+
+				t.log.Info("writing packages")
+				wrote, err := ff.Write(rtpBuf[:packageSize])
+				if err != nil {
+					t.log.Error(fmt.Errorf("failed to write track package", err).Error())
 				}
+				t.log.Info(fmt.Sprintf("wrote %s", wrote))
+				// rtpPacket, _, err := track.ReadRTP()
+				// if err != nil {
+				// 	t.log.Error("failed to read track package")
+				// 	return
+				// }
+				// if err := oggFile.WriteRTP(rtpPacket); err != nil {
+				// 	t.log.Error("failed to write ogg file")
+				// 	return
+				// }
+				// if len(buffer) >= 10000000000 {
+				// 	t.log.Info(fmt.Sprintf("Buffer is nearly full. Current size: %d", len(buffer)))
+				// 	result, err := ff.Write(buffer)
+				// 	if err != nil {
+				// 		t.log.Error("failed to create audio")
+				// 		return
+				// 	}
+				// 	outputFile, err := os.Create("out.mp3") // create new file
+				// 	if err != nil {
+				// 		t.log.Error("failed to create audio file")
+				// 		return
+				// 	}
+				// 	defer outputFile.Close()
+				// 	_, err = outputFile.Write(result.Bytes()) // write result buffer to file
+				// 	if err != nil {
+				// 		t.log.Error("failed to write audio file")
+				// 		return
+				// 	}
+				// }
 			}
 		}
 		return
--- pkg/ffmpeg/main.go
+++ pkg/ffmpeg/main.go
@@ -12,37 +12,47 @@ func NewFfmpeg() (*Ffmpeg, error) {
 }
 
 type Ffmpeg struct {
-	cmd   *exec.Cmd
-	stdin io.WriteCloser
+	cmd     *exec.Cmd
+	stdin   io.WriteCloser
+	started bool
 }
 
-func (f *Ffmpeg) Init() {
+func (f *Ffmpeg) Init() error {
+	// f.cmd = exec.Command("ffmpeg", "-y", // Yes to all
+	// 	//"-hide_banner", "-loglevel", "panic", // Hide all logs
+	// 	"-i", "pipe:0", // take stdin as input
+	// 	"-map_metadata", "-1", // strip out all (mostly) metadata
+	// 	"-c:a", "libmp3lame", // use mp3 lame codec
+	// 	"-vsync", "2", // suppress "Frame rate very high for a muxer not efficiently supporting it"
+	// 	"-b:a", "128k", // Down sample audio birate to 128k
+	// 	"-f", "mp3", // using mp3 muxer (IMPORTANT, output data to pipe require manual muxer selecting)
+	// 	"pipe:1", // output to stdout
+	// )
 	f.cmd = exec.Command("ffmpeg", "-y", // Yes to all
 		//"-hide_banner", "-loglevel", "panic", // Hide all logs
-		"-i", "pipe:0", // take stdin as input
-		"-map_metadata", "-1", // strip out all (mostly) metadata
-		"-c:a", "libmp3lame", // use mp3 lame codec
-		"-vsync", "2", // suppress "Frame rate very high for a muxer not efficiently supporting it"
-		"-b:a", "128k", // Down sample audio birate to 128k
-		"-f", "mp3", // using mp3 muxer (IMPORTANT, output data to pipe require manual muxer selecting)
-		"pipe:1", // output to stdout
+		"-protocol_whitelist", "pipe,file,udp,rtp", "-i", "-", "-acodec", "pcm_s16le", "pipe:1",
 	)
-}
-
-func (f *Ffmpeg) Read(buf []byte) (*bytes.Buffer, error) {
-	resultBuffer := bytes.NewBuffer(make([]byte, 5*1024*1024)) // pre allocate 5MiB buffer
 	f.cmd.Stdout = os.Stdout
 	f.cmd.Stderr = os.Stderr
 	stdin, err := f.cmd.StdinPipe() // Open stdin pipe
+	if err != nil {
+		return err
+	}
 	f.stdin = stdin
-	if err != nil {
-		return nil, err
+	f.started = false
+	return nil
+}
+
+func (f *Ffmpeg) Write(buf []byte) (*bytes.Buffer, error) {
+	resultBuffer := bytes.NewBuffer(make([]byte, 5*1024*1024)) // pre allocate 5MiB buffer
+	if f.started == false {
+		err := f.cmd.Start()
+		if err != nil {
+			return nil, err
+		}
+		f.started = true
 	}
-	err = f.cmd.Start()
-	if err != nil {
-		return nil, err
-	}
-	_, err = stdin.Write(buf) // pump audio data to stdin pipe
+	_, err := f.stdin.Write(buf) // pump audio data to stdin pipe
 	if err != nil {
 		return nil, err
 	}
